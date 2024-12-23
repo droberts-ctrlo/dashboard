0c157ba0e95685208be5b1e4986c8778bbac3d8c
Dave Roberts <d.roberts@ctrlo.com>
2024-12-23 12:46:26 +0000

    Initial project creation

---
+++ dashboard-backend/src/test/java/blog/davetheitguy/dashboard_backend/DashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.davetheitguy.dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
