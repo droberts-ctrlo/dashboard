cb86134e70fc477055c2fba44e878c7df6ed1750
Dave Roberts <d.roberts@ctrlo.com>
2024-12-23 16:19:23 +0000

    Exeperements started

--- dashboard-frontend/src/app/app.component.ts
+++ dashboard-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ktdTrackById } from '@katoid/angular-grid-layout';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dashboard-frontend';
+  cols = 2;
+  rowHeight = 200;
+
+  layout = [
+    {id: '0', x: 0, y: 0, w: 1, h: 1, text: "This is an item"},
+    {id: '1', x: 1, y: 0, w: 1, h: 1, img:"https://via.placeholder.com/150"},
+  ];
+
+  trackById = ktdTrackById;
 }
