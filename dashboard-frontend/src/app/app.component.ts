0dc661c135f00eea7dd6a816afa1eabedf11486f
Dave Roberts <d.roberts@ctrlo.com>
2024-12-25 15:49:04 +0000

    Updated types to allow tables

--- dashboard-frontend/src/app/app.component.ts
+++ dashboard-frontend/src/app/app.component.ts
@@ -1,6 +1,22 @@
 import { Component } from '@angular/core';
 import { ktdTrackById } from '@katoid/angular-grid-layout';
 
+interface Table {
+  headers: string[],
+  rows: any[][]
+};
+
+interface Content {
+  id: string,
+  x: number,
+  y: number,
+  w: number,
+  h: number,
+  type: string,
+  content?: string,
+  table?: Table
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,22 +28,28 @@ export class AppComponent {
   rowHeight = 200;
 
   layout = [
-    {id: '0', x: 0, y: 0, w: 1, h: 1},
-    {id: '1', x: 1, y: 0, w: 1, h: 1},
+    { id: '0', x: 0, y: 0, w: 1, h: 1 },
+    { id: '1', x: 1, y: 0, w: 1, h: 1 },
+    { id: '2', x: 0, y: 1, w: 2, h: 1 },
   ];
 
   content = [
-    {id: '0',type: 'text', content: "This is an item"},
-    {id: '1', type: 'image', content:"https://via.placeholder.com/150"},
+    { id: '0', type: 'text', content: "This is an item" },
+    { id: '1', type: 'image', content: "https://via.placeholder.com/150" },
+    { id: '2', type: 'table', content: { headers: ['Name', 'Age'], rows: [['Alice', 25], ['Bob', 30]] } }
   ]
 
   get items() {
-    const result = [];
-    for(let i =0 ;i < this.layout.length; i++) {
+    const result: Content[] = [];
+    for (let i = 0; i < this.layout.length; i++) {
       const item = this.layout[i];
-      const content: {id: string, type:string, content: string} | undefined = this.content.find(c => c.id === item.id);
-      if(!content) continue;
-      result.push({...item, type: content.type, content: content.content})
+      const content: { id: string, type: string, content: string | Table } | undefined = this.content.find(c => c.id === item.id);
+      if (!content) continue;
+      if (content.type === 'table') {
+        result.push({ ...item, type: content.type, table: content.content as Table });
+      } else {
+        result.push({ ...item, type: content.type, content: content.content as string });
+      }
     }
     return result;
   }
